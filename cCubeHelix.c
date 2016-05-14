3d2d8021c081d63833958ebf1a7225d778054a2e
thegrandmother <henrik.sommerland@gmail.com>
2016-05-14 14:59:39 +0200

    added license

--- cCubeHelix.c
+++ cCubeHelix.c
@@ -1,82 +1,47 @@
-/**C=====================================================================72
-C Calculates a "cube helix" colour table. The colours are a tapered
-C helix around the diagonal of the [R,G,B] colour cube, from black
-C [0,0,0] to white [1,1,1] Deviations away from the diagonal vary
-C quadratically, increasing from zero at black, to a maximum, then
-C decreasing to zero at white, all the time rotating in colour.
-C
-C The input parameters controlling the colour helix are:
-C
-C START colour (1=red, 2=green, 3=blue; e.g. 0.5=purple);
-C ROTS rotations in colour (typically -1.5 to 1.5, e.g. -1.0
-C is one blue->green->red cycle);
-C HUE for hue intensity scaling (in the range 0.0 (B+W) to 1.0
-C to be strictly correct, larger values may be OK with
-C particular start/end colours);
-C GAMMA set the gamma correction for intensity.
-C
-C The routine returns a colour table NLEV elements long in RED, GRN
-C and BLU (each element in the range 0.0 to 1.0), and the numbers,
-C NLO and NHI, of red, green or blue values that had to be clipped
-C because they were too low or too high.
-C---------------------------------------------------------------------72
-C Dave Green --- MRAO --- 2011 June 13th
-C---------------------------------------------------------------------72
-C See:
-C Green, D. A., 2011, Bulletin of the Astronomical Society of India,
-C Vol.39, p.289
-C---------------------------------------------------------------------72
-SUBROUTINE CUBHLX(START,ROTS,HUE,GAMMA,NLEV,RED,GRN,BLU,NLO,NHI)
-C ================================================================
-C
-INTEGER NLEV,I,NLO,NHI
-REAL START,ROTS,HUE,GAMMA
-REAL RED(NLEV),GRN(NLEV),BLU(NLEV)
-REAL PI,FRACT,ANGLE,AMP
-C
-PI=4.0*ATAN(1.0)
-NLO=0
-NHI=0
-C
-DO 1000 I=1,NLEV
-FRACT=FLOAT(I-1)/FLOAT(NLEV-1)
-ANGLE=2*PI*(START/3.0+1.0+ROTS*FRACT)
-FRACT=FRACT**GAMMA
-AMP=HUE*FRACT*(1-FRACT)/2.0
-RED(I)=FRACT+AMP*(-0.14861*COS(ANGLE)+1.78277*SIN(ANGLE))
-GRN(I)=FRACT+AMP*(-0.29227*COS(ANGLE)-0.90649*SIN(ANGLE))
-BLU(I)=FRACT+AMP*(+1.97294*COS(ANGLE))
-C
-IF(RED(I).LT.0.0)THEN
-RED(I)=0.0
-NLO=NLO+1
-ENDIF
-IF(GRN(I).LT.0.0)THEN
-GRN(I)=0.0
-NLO=NLO+1
-ENDIF
-IF(BLU(I).LT.0.0)THEN
-BLU(I)=0.0
-NLO=NLO+1
-ENDIF
-C
-IF(RED(I).GT.1.0)THEN
-RED(I)=1.0
-NHI=NHI+1
-ENDIF
-IF(GRN(I).GT.1.0)THEN
-GRN(I)=1.0
-NHI=NHI+1
-ENDIF
-IF(BLU(I).GT.1.0)THEN
-BLU(I)=1.0
-NHI=NHI+1
-ENDIF
-1000 CONTINUE
-C
-RETURN
-END**/
-
+/**
+ * A basic C implementation of the cubehelix color scheme by David Green.
+ *
+ * Documentation can be found in cCubeHelix.h
+ *
+ * Author: Henrik Sommerland
+ * Main: henrik.sommerland@gmail.com
+ *
+ * This code is based upon the FORTRAN 77 implementation
+ * provided in the paper:
+ *
+ * A colour scheme for the display of astronomical intensity images
+ * By: D. A. Green
+ *
+ * http://arxiv.org/abs/1108.5083
+ *
+ *
+ * 
+ *
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org/>
+ **/
 
 #include <stdlib.h>
 #include <stdio.h>
